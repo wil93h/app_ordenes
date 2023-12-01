3c00955c8e1d1428b13a5d9f90598d1bf4774f41
rene_barrientos <rene.barrientos@datasys.la>
2023-12-01 08:24:23 -0600

    first commit

---
+++ resources/js/main.jsx
@@ -0,0 +1,9 @@
+import {StrictMode} from 'react'
+import ReactDOM from 'react-dom';
+// import ExampleComponent from './components/ExampleComponent';
+
+ReactDOM.render(
+<StrictMode>
+    <h1>hola2</h1>
+</StrictMode>
+, document.getElementById('root'));
